10676ac38715699ebfcc66c0d32197cef694c55c
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2019-11-21 23:47:43 -0800

    testing heroku

--- TaskOrganizer/myapp/form.py
+++ TaskOrganizer/myapp/form.py
@@ -1,19 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
 
 
-class Login(FlaskForm):
-    user_name = StringField('User Name')
+class LoginForm(FlaskForm):
+    username = StringField('User Name')
     password = PasswordField('Password')
-    signup = SubmitField('Register')
     login = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    user_name = StringField('User Name', validators=[DataRequired()])
+    username = StringField('User Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
